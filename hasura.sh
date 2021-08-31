345a527a400431e58f8922d363a2d5423b92b522
Hugo Wood <hugo.wood@zenika.com>
2021-08-31 22:07:02 +0200

    🛠 add dev container

--- hasura.sh
+++
@@ -1,1 +0,0 @@
-MSYS_NO_PATHCONV=1 docker run -it --rm --entrypoint /bin/hasura-cli --net host -v $(pwd):/app hasura/graphql-engine:v2.0.2.cli-migrations-v3 --project /app/hasura --skip-update-check $*
