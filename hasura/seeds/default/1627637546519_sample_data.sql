4344372791faecaf9b708780780133b68a02f7e2
Hugo Wood <hugo.wood@zenika.com>
2021-09-01 22:29:47 +0200

    🗃 add more sample data

--- hasura/seeds/default/1627637546519_sample_data.sql
+++ hasura/seeds/default/1627637546519_sample_data.sql
@@ -17,13 +17,16 @@ insert into
   consultant (matricule, nom, matricule_manager)
   values
     ('RPA', 'Robert Patrick', 'RMA'),
-    ('GDU', 'Géraldine Dumur', 'RMA')
+    ('GDU', 'Géraldine Dumur', 'RMA'),
+    ('SFR', 'Samy Franklin', 'RMA'),
+    ('YAD', 'Yvonne Adélaïde', 'RMA')
   on conflict (matricule)
     do update set nom = excluded.nom, matricule_manager = excluded.matricule_manager;
 
 insert into
   demi_journee_de_cra (matricule_consultant, date, demi_journee, code_projet)
   values
+    ('RPA', '2021-08-01', 'matin', 'P1'),
     ('GDU', '2021-09-01', 'matin', 'P1'),
     ('GDU', '2021-09-01', 'apres_midi', 'P1'),
     ('GDU', '2021-09-02', 'matin', 'P1'),
@@ -83,6 +86,12 @@ insert into
     ('GDU', '2021-09-29', 'matin', 'P2'),
     ('GDU', '2021-09-29', 'apres_midi', 'P2'),
     ('GDU', '2021-09-30', 'matin', 'P2'),
-    ('GDU', '2021-09-30', 'apres_midi', 'P2')
+    ('GDU', '2021-09-30', 'apres_midi', 'P2'),
+    ('SFR', '2021-09-01', 'matin', 'P1'),
+    ('SFR', '2021-09-01', 'apres_midi', 'P1'),
+    ('SFR', '2021-09-02', 'matin', 'P1'),
+    ('SFR', '2021-09-02', 'apres_midi', 'P1'),
+    ('SFR', '2021-09-03', 'matin', 'P1'),
+    ('SFR', '2021-09-03', 'apres_midi', 'P1')
   on conflict (matricule_consultant, date, demi_journee)
     do update set code_projet = excluded.code_projet;
