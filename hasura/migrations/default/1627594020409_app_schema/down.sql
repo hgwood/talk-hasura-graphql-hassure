062f62bdb82a4ff2e77596536350ae9c5af55507
Hugo Wood <hx4.5@free.fr>
2021-09-22 00:29:22 +0200

    🐛 add missing drop in down migration

--- hasura/migrations/default/1627594020409_app_schema/down.sql
+++ hasura/migrations/default/1627594020409_app_schema/down.sql
@@ -1,3 +1,4 @@
+drop function if exists nombre_jours_dans_mois(integer, integer);
 drop table if exists validation_feuille_temps;
 drop table if exists journee_feuille_temps;
 drop table if exists consultant;
