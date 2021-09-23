910ebadcbfef1d133ab7faf43607e385f632ae43
Hugo Wood <hugo.wood@zenika.com>
2021-09-23 12:25:55 +0200

    more vertical formatting

--- hasura/migrations/default/1630531266487_can_validate_only_if_complete/up.sql
+++ hasura/migrations/default/1630531266487_can_validate_only_if_complete/up.sql
@@ -5,12 +5,18 @@ create function journee_feuille_temps_feuille_complete
   returns boolean
   language sql stable
   as $$
-    select nombre_jours_dans_mois(journee_feuille_temps_row.annee, journee_feuille_temps_row.mois) = (
-      select count(*)
-      from journee_feuille_temps
-      where
-        annee = journee_feuille_temps_row.annee
-        and mois = journee_feuille_temps_row.mois
-        and matricule_consultant = journee_feuille_temps_row.matricule_consultant
-    );
+    select
+      nombre_jours_dans_mois(
+        journee_feuille_temps_row.annee,
+        journee_feuille_temps_row.mois
+      )
+      =
+      (
+        select count(*)
+        from journee_feuille_temps
+        where
+          annee = journee_feuille_temps_row.annee
+          and mois = journee_feuille_temps_row.mois
+          and matricule_consultant = journee_feuille_temps_row.matricule_consultant
+      );
   $$
