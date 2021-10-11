73c530d969327afa9bf596de273f0e002a9e91c2
Hugo Wood <hx4.5@free.fr>
2021-10-11 21:10:57 +0200

    🗃 make migration idempotent

--- hasura/migrations/default/1630523886529_demi_journee_de_cra_annee_mois/up.sql
+++ hasura/migrations/default/1630523886529_demi_journee_de_cra_annee_mois/up.sql
@@ -1,6 +1,6 @@
 alter table journee_feuille_temps
-  drop column annee,
-  drop column mois;
+  drop column if exists annee,
+  drop column if exists mois;
 
 alter table journee_feuille_temps
   add column annee integer generated always as (extract(year from date)) stored,
