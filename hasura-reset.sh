f0dcd71245f1d51e8309acecbc3d6b12c9281eb8
Hugo Wood <hx4.5@free.fr>
2021-10-11 21:19:18 +0200

    ✨ add reset script

---
+++ hasura-reset.sh
@@ -0,0 +1,1 @@
+MSYS_NO_PATHCONV=1 docker run -it --rm --entrypoint /bin/hasura-cli --net host -v $(pwd):/app hasura/graphql-engine:v2.0.7.cli-migrations-v3 --project /app/hasura-reset --skip-update-check metadata apply
